2a16d5d62d598197c3d3f48cfc748e74c1fd2bd0
Natan Martins dos Santos <natantcr@gmail.com>
2022-10-25 22:29:15 -0300

    Configured a simple server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>My list of tasks</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("Server deployed: localhost:3000");
+});
